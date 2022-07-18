6b1b5fab3ad685e78a63ab55d3244854662ee5df
KimSwings <109517689+KimSwings@users.noreply.github.com>
2022-07-18 10:10:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def main():
 """
     )
     await call_py.join_chat(GROUP_ID)
-    await call_py.send_message(GROUP_ID, "I Used Your Code For Music")
+    await call_py.send_message(GROUP_ID, "I work for True King Plunderer")
     await idle()
     await arq.close()
 
