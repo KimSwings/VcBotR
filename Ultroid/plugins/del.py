bd949c4ee4ddc219bc889862789755477926b112
KimSwings <109517689+KimSwings@users.noreply.github.com>
2022-07-21 09:15:31 +0000

    Create del.py

---
+++ Ultroid/plugins/del.py
@@ -0,0 +1,15 @@
+@ultroid_cmd(
+    pattern="del$",
+)
+async def delete_it(delme):
+    msg_src = await delme.get_reply_message()
+    if delme.reply_to_msg_id:
+        try:
+            await msg_src.delete()
+            await delme.delete()
+        except BaseException:
+            await eod(
+                delme,
+                f"Couldn't delete the message.\n\n**ERROR:**\n`{str(e)}`",
+                time=5,
+            )
