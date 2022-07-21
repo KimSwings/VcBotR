f2ab5de8b6983380046210b644ab6051a102116e
KimSwings <109517689+KimSwings@users.noreply.github.com>
2022-07-21 09:54:59 +0000

    Update del.py

--- Userbot/del.py
+++ Userbot/del.py
@@ -2,8 +2,8 @@
 @authorised_users_only
 async def ping(client, m: Message):
  replied = m.reply_to_message
-    chat_id = m.chat.id
-    m.chat.title
+    #chat_id = m.chat.id
+    #m.chat.title
     if replied:
         if replied.audio or replied.voice:
             await m.delete()
