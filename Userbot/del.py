94a6a9aab5e4b39391bbf6909fb20c5f81d13d83
KimSwings <109517689+KimSwings@users.noreply.github.com>
2022-07-21 09:53:57 +0000

    Create del.py

---
+++ Userbot/del.py
@@ -0,0 +1,9 @@
+@Client.on_message(filters.command(["del"], prefixes=f"{HNDLR}"))
+@authorised_users_only
+async def ping(client, m: Message):
+ replied = m.reply_to_message
+    chat_id = m.chat.id
+    m.chat.title
+    if replied:
+        if replied.audio or replied.voice:
+            await m.delete()
