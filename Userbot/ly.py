0143fbbcddbc5890bc2e467e72f0327a88a28395
KimSwings <109517689+KimSwings@users.noreply.github.com>
2022-07-27 15:49:08 +0000

    Create ly.py

---
+++ Userbot/ly.py
@@ -0,0 +1,18 @@
+s (15 sloc)  663 Bytes
+
+from pyrogram import Client, filters
+from config import HNDLR, bot
+import re
+import requests
+r = requests.get('http://www.songlyrics.com/index.php?section=search&searchW=darling+nikki&submit=Search')
+k = re.findall(r'href="http://www.songlyrics.com/([^"]+)', r.text)
+
+@Client.on_message(filters.command(["lyrics", "l"], prefixes=f"{HNDLR}"))
+async def _(client, message):
+    lel = await message.reply("Searching For Lyrics...")
+    
+x = requests.get(f'http://www.songlyrics.com/{k[1]}')
+m = re.search(r'iComment-text">([^=]+)', x.text)
+res = m[0].replace('<br />', '')
+song = re.search(r'>([^<]+)', res) 
+print(song[0])
