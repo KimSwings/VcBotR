b1553f937931355a28b1475d0f7b08c174bc4635
KimSwings <109517689+KimSwings@users.noreply.github.com>
2022-07-27 19:36:12 +0000

    Update ly.py

--- Userbot/ly.py
+++ Userbot/ly.py
@@ -6,7 +6,7 @@ from PyLyrics import PyLyrics
 r = requests.get('http://www.songlyrics.com/index.php?section=search&searchW=""&submit=Search')
 k = re.findall(r'href="http://www.songlyrics.com/([^"]+)', r.text)
 
-x = requests.get(f'http://www.songlyrics.com/{k[1]}')
+x = requests.get(f'http://www.songlyrics.com/{k["1"]}'))
 m = re.search(r'iComment-text">([^=]+)', x.text)
 res = m[0].replace('<br />', '')
 song = re.search(r'>([^<]+)', res) 
