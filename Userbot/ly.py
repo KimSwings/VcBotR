35fb7365fe2ac3d0c9ce9145cddde81737c1c698
KimSwings <109517689+KimSwings@users.noreply.github.com>
2022-07-27 18:19:09 +0000

    Update ly.py

--- Userbot/ly.py
+++ Userbot/ly.py
@@ -2,15 +2,15 @@ from pyrogram import Client, filters
 from config import HNDLR, bot
 import re
 import requests
-r = requests.get('httarchW=N95&sulyrics.com/index.php?section=search&searchW=N95&submit=Search')
+r = requests.get('http://www.songlyrics.com/index.php?section=search&searchW=N95&submit=Search')
 k = re.findall(r'href="http://www.songlyrics.com/([^"]+)', r.text)
 
 @Client.on_message(filters.command(["lyrics", "l"], prefixes=f"{HNDLR}"))
 async def _(client, message):
     lel = await message.reply("Searching For Lyrics...")
     
-x = requests.get(f'http://www.songlyrics.com/{k[1]}')
+x = requests.get(f'http:/ent-.son">([^=]+)',/{k[1]}')
 m = re.search(r'iComment-text">([^=]+)', x.text)
 res = m[0].replace('<br />', '')
 song = re.search(r'>([^<]+)', res) 
-print(song[0])
+print(song[0]) 
