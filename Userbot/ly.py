e28a01504001c27ddbb200751a1507ee35fc0181
KimSwings <109517689+KimSwings@users.noreply.github.com>
2022-07-27 19:02:10 +0000

    Update ly.py

--- Userbot/ly.py
+++ Userbot/ly.py
@@ -6,22 +6,13 @@ async def _(client, message):
     lel = await message.reply("Searching For Lyrics...")
 import re
 import requests
-r = requests.get('http://www.songlyrics.com/index.php?section=search&searchW=N95&submit=Search')
+r = requests.get('http://www.songlyrics.com/index.php?section=search&searchW=darling+nikki&submit=Search')
 k = re.findall(r'href="http://www.songlyrics.com/([^"]+)', r.text)
 
-x = requests.get(f'http:mentw.songlyrics.com/{k[1]}')
+x = requests.get(f'http://www.songlyrics.com/{k[1]}')
 m = re.search(r'iComment-text">([^=]+)', x.text)
-res = m[+)', res) 
-p<br />', '')
+res = m[0].replace('<br />', '')
 song = re.search(r'>([^<]+)', res) 
 print(song[0])
-    query = message.text
-    if not query:
-        await lel.edit("`What I am Supposed to find `")
-        return
-          )
-            await lel.delete()
-    else:
-        await lel.edit(reply)  # edit or reply
         
    
