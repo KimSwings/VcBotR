68a390ba49d16a988980009b1e298fed565531e0
KimSwings <109517689+KimSwings@users.noreply.github.com>
2022-07-28 02:40:09 +0000

    Update ly.py

--- Userbot/ly.py
+++ Userbot/ly.py
@@ -9,15 +9,17 @@ k = re.findall(r'href="http://www.songlyrics.com/([^"]+)', r.text)
 @Client.on_message(filters.command(["lyrics", "l"], prefixes=f"{HNDLR}"))
 async def _(client, message):
     lel = await message.reply("Searching For Lyrics...")
-    query = message.text
-    if not query:
-        await lel.edit("`What I am Supposed to find `")
-        return
 pl=k[1]
 x = requests.get(f'http://www.songlyrics.com/{pl}')
 m = re.search(r'iComment-text">([^=]+)', x.text)
 res = m[0].replace('<br />', '')
 song = re.search(r'>([^<]+)', res) 
 print(song[0])
+    query = message.text
+    if not query:
+        await lel.edit("`What I am Supposed to find `")
+        return,
+        )
+
 
    
