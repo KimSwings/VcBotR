d959499c315429d44b9058ff81ed1dfdbfb25316
KimSwings <109517689+KimSwings@users.noreply.github.com>
2022-09-25 05:27:48 +0000

    Update play.py

--- Userbot/play.py
+++ Userbot/play.py
@@ -20,182 +20,134 @@ from Userbot.helpers.queues import QUEUE, add_to_queue, get_queue
 
 # music player
 def ytsearch(query):
-    try:
-        search = VideosSearch(query, limit=1)
-        for r in search.result()["result"]:
-            ytid = r["id"]
-            if len(r["title"]) > 34:
-                songname = r["title"][:35] + "..."
-            else:
-                songname = r["title"]
-            url = f"https://www.youtube.com/watch?v={ytid}"
-        return [songname, url]
-    except Exception as e:
-        print(e)
-        return 0
+   try:
+      search = VideosSearch(query, limit=1)
+      for r in search.result()["result"]:
+         ytid = r['id']
+         if len(r['title']) > 34:
+            songname = r['title'][:35] + "..."
+         else:
+            songname = r['title']
+         url = f"https://www.youtube.com/watch?v={ytid}"
+      return [songname, url]
+   except Exception as e:
+      print(e)
+      return 0
 
-
+# YTDL
+# https://github.com/pytgcalls/pytgcalls/blob/dev/example/youtube_dl/youtube_dl_example.py
 async def ytdl(link):
-    proc = await asyncio.create_subprocess_exec(
-        "yt-dlp",
-        "-g",
-        "-f",
-        # CHANGE THIS BASED ON WHAT YOU WANT
-        "bestaudio",
-        f"{link}",
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    stdout, stderr = await proc.communicate()
-    if stdout:
-        return 1, stdout.decode().split("\n")[0]
-    else:
-        return 0, stderr.decode()
+   proc = await asyncio.create_subprocess_exec(
+       'youtube-dl',
+       '-g',
+       '-f',
+       # CHANGE THIS BASED ON WHAT YOU WANT
+       'bestaudio',
+       f'{link}',
+       stdout=asyncio.subprocess.PIPE,
+       stderr=asyncio.subprocess.PIPE,
+   )
+   stdout, stderr = await proc.communicate()
+   if stdout:
+      return 1, stdout.decode().split('\n')[0]
+   else:
+      return 0, stderr.decode()
 
 
-
-# video player
-def ytsearch(query):
-    try:
-        search = VideosSearch(query, limit=1).result()
-        data = search["result"][0]
-        songname = data["title"]
-        url = data["link"]
-        duration = data["duration"]
-        thumbnail = f"https://i.ytimg.com/vi/{data['id']}/hqdefault.jpg"
-        return [songname, url, duration, thumbnail]
-    except Exception as e:
-        print(e)
-        return 0
-
-
-async def ytdl(link):
-    proc = await asyncio.create_subprocess_exec(
-        "yt-dlp",
-        "-g",
-        "-f",
-        # CHANGE THIS BASED ON WHAT YOU WANT
-        "medium[height<=?720][width<=?1280]",
-        f"{link}",
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    stdout, stderr = await proc.communicate()
-    if stdout:
-        return 1, stdout.decode().split("\n")[0]
-    else:
-        return 0, stderr.decode()
-
-
-@Client.on_message(filters.command(["play", "p"], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(['play'], prefixes=f"{HNDLR}"))
 async def play(client, m: Message):
-    replied = m.reply_to_message
-    chat_id = m.chat.id
-    m.chat.title
-    if replied:
-        if replied.audio or replied.voice:
-            await m.delete()
-            huehue = await replied.reply("**🔄 Searching**")
-            dl = await replied.download()
-            link = replied.link
-            if replied.audio:
-                if replied.audio.title:
-                    songname = replied.audio.title[:35] + "..."
-                else:
-                    songname = replied.audio.file_name[:35] + "..."
-            elif replied.voice:
-                songname = "Voice Note"
-            if chat_id in QUEUE:
-                pos = add_to_queue(chat_id, songname, dl, link, "Audio", 0)
-                await huehue.delete()
-                # await m.reply_to_message.delete()
-                await m.reply_photo(
-                    photo="https://telegra.ph/file/56d4b720b2b7a12963ee8.jpg",
-                    caption=f"""
-**#⃣ Song In Queue Ke {pos}
-🏷️ Title: [{songname}]({link})
-💬 Chat ID: {chat_id}
-🎧 On Request: {m.from_user.mention}**
-""",
-                )
+   replied = m.reply_to_message
+   chat_id = m.chat.id
+   if replied:
+      if replied.audio or replied.voice:
+         huehue = await replied.reply("`Downloading`")
+         dl = await replied.download()
+         link = replied.link
+         if replied.audio:
+            if replied.audio.title:
+               songname = replied.audio.title[:35] + "..."
             else:
-                await call_py.join_group_call(
-                    chat_id,
-                    AudioPiped(
-                        dl,
-                    ),
-                    stream_type=StreamType().pulse_stream,
-                )
-                add_to_queue(chat_id, songname, dl, link, "Audio", 0)
-                await huehue.delete()
-                # await m.reply_to_message.delete()
-                await m.reply_photo(
-                    photo="https://telegra.ph/file/ebf2ad7ee9bfcca8c4954.jpg",
-                    caption=f"""
-**▶ Start Playing Song
-🏷️ Title: [{songname}]({link})
-💬 Chat ID: {chat_id}
-🎧 Requested By: {m.from_user.mention}**
-""",
-                )
-
-    else:
-        if len(m.command) < 2:
-            await m.reply("Reply to Audio Files or provide something for Searches")
-        else:
-            await m.delete()
-            huehue = await m.reply("🔎 Searching")
+               songname = replied.audio.file_name[:35] + "..."
+         elif replied.voice:
+            songname = "Voice Note"
+         if chat_id in QUEUE:
+            pos = add_to_queue(chat_id, songname, dl, link)
+            await huehue.edit(f"Queued at **#{pos}**")
+         else:
+            await call_py.join_group_call(
+               chat_id,
+               AudioPiped(
+                  dl,
+               ),
+               stream_type=StreamType().pulse_stream,
+            )
+            add_to_queue(chat_id, songname, dl, link)
+            await huehue.edit(f"**Started Playing ▶** \n**🎧 SONG** : [{songname}]({link}) \n**💬 CHAT** : `{chat_id}`", disable_web_page_preview=True)
+      else:
+         if len(m.command) < 2:
+            await m.reply("`Reply to an Audio File or give something to Search`")
+         else:
+            huehue = await m.reply("`Searching...`")
             query = m.text.split(None, 1)[1]
             search = ytsearch(query)
-            if search == 0:
-                await huehue.edit("`Found Nothing for Given Query`")
+            if search==0:
+               await huehue.edit("`Found Nothing for the Given Query`")
             else:
-                songname = search[0]
-                url = search[1]
-                duration = search[2]
-                thumbnail = search[3]
-                hm, ytlink = await ytdl(url)
-                if hm == 0:
-                    await huehue.edit(f"**YTDL ERROR ⚠️** \n\n`{ytlink}`")
-                else:
-                    if chat_id in QUEUE:
-                        pos = add_to_queue(chat_id, songname, ytlink, url, "Audio", 0)
-                        await huehue.delete()
-                        # await m.reply_to_message.delete()
-                        await m.reply_photo(
-                            photo=f"{thumbnail}",
-                            caption=f"""
-**#⃣ Song In Queue  {pos}
-🏷️ Titel: [{songname}]({url})
-⏱️ Duration: {duration}
-💬 Chat ID: {chat_id}
-🎧 On request: {m.from_user.mention}**
-""",
+               songname = search[0]
+               url = search[1]
+               hm, ytlink = await ytdl(url)
+               if hm==0:
+                  await huehue.edit(f"**YTDL ERROR ⚠️** \n\n`{ytlink}`")
+               else:
+                  if chat_id in QUEUE:
+                     pos = add_to_queue(chat_id, songname, ytlink, url)
+                     await huehue.edit(f"Queued at **#{pos}**")
+                  else:
+                     try:
+                        await call_py.join_group_call(
+                           chat_id,
+                           AudioPiped(
+                              ytlink,
+                           ),
+                           stream_type=StreamType().pulse_stream,
                         )
-                    else:
-                        try:
-                            await call_py.join_group_call(
-                                chat_id,
-                                AudioPiped(
-                                    ytlink,
-                                ),
-                                stream_type=StreamType().pulse_stream,
-                            )
-                            add_to_queue(chat_id, songname, ytlink, url, "Audio", 0)
-                            await huehue.delete()
-                            # await m.reply_to_message.delete()
-                            await m.reply_photo(
-                                photo=f"{thumbnail}",
-                                caption=f"""
-**▶ Start Playing Song
-🏷️ Title: [{songname}]({url})
-⏱️ Duration: {duration}
-💬 Chat ID: {chat_id}
-🎧 On request: {m.from_user.mention}**
-""",
-                            )
-                        except Exception as ep:
-                            await huehue.edit(f"`{ep}`")
+                        add_to_queue(chat_id, songname, ytlink, url)
+                        await huehue.edit(f"**Started Playing ▶** \n**🎧 SONG** : [{songname}]({url}) \n**💬 CHAT** : `{chat_id}`", disable_web_page_preview=True)
+                     except Exception as ep:
+                        await huehue.edit(f"`{ep}`")
+            
+   else:
+         if len(m.command) < 2:
+            await m.reply("`Reply to an Audio File or give something to Search`")
+         else:
+            huehue = await m.reply("`Searching...`")
+            query = m.text.split(None, 1)[1]
+            search = ytsearch(query)
+            if search==0:
+               await huehue.edit("`Found Nothing for the Given Query`")
+            else:
+               songname = search[0]
+               url = search[1]
+               hm, ytlink = await ytdl(url)
+               if hm==0:
+                  await huehue.edit(f"**YTDL ERROR ⚠️** \n\n`{ytlink}`")
+               else:
+                  if chat_id in QUEUE:
+                     pos = add_to_queue(chat_id, songname, ytlink, url)
+                     await huehue.edit(f"Queued at **#{pos}**")
+                  else:
+                     try:
+                        await call_py.join_group_call(
+                           chat_id,
+                           AudioPiped(
+                              ytlink,
+                           ),
+                           stream_type=StreamType().pulse_stream,
+                        )
+                        add_to_queue(chat_id, songname, ytlink, url)
+                        await huehue.edit(f"**Started Playing ▶** \n**🎧 SONG** : [{songname}]({url}) \n**💬 CHAT** : `{chat_id}`", disable_web_page_preview=True)
+                     except Exception as ep:
+                        await huehue.edit(f"`{ep}`")
 
 
 @Client.on_message(filters.command(["vplay", "vp"], prefixes=f"{HNDLR}"))
